8ffac5cc7e6dd20452694ca8da29ebcc7618c3d5
BryanAim <isale.bryan@gmail.com>
2021-05-05 07:45:53 +0000

    add toggle class to make image big and small

--- behavior.js
+++ behavior.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var thumbnailElement = document.getElementById('smart_thumbnail');
   
   thumbnailElement.addEventListener("click", function() {
-    thumbnailElement.className = "";
-    
+    // thumbnailElement.className = "";
+    thumbnailElement.classList.toggle("small");
+
   })
+  // if (thumbnailElement.className == "") {
+  //   thumbnailElement.addEventListener("click", function() {
+  //     thumbnailElement.className = "small";
+  //   })
+  // }
 });
